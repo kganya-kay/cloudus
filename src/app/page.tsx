cded29a85909bf49fa6bd6c4caef0fe33bedadcc
kganya <kganyakekana@gmail.com>
2024-10-03 09:07:02 -0700

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,28 +5,51 @@ import { getServerAuthSession } from "~/server/auth";
 import { api, HydrateClient } from "~/trpc/server";
 
 export default async function Home() {
-  const hello = await api.post.hello({ text: "from tRPC" });
+  const hello = await api.post.hello({ text: "from Cloudus" });
   const session = await getServerAuthSession();
 
   void api.post.getLatest.prefetch();
 
   return (
     <HydrateClient>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#0f766e] to-[#177a72] text-white">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
           <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-            Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
+            C L O U D <span className="text-[hsl(0,0%,0%)]">U S</span> 
           </h1>
+          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
+            <Link
+              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
+              href="#"
+              target="_blank"
+            >
+              <h3 className="text-2xl font-bold">Web Design & Development →</h3>
+              <div className="text-lg">
+              develop optimized Web browsing solutions to ensure your end-users can quickly and conveniently accomplish their tasks.
+              </div>
+            </Link>
+            <Link
+              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20 "
+              href="https://create.t3.gg/en/introduction"
+              target="_blank"
+            >
+              <h3 className="text-2xl font-bold">Design and Printing →</h3>
+              <div className="text-lg">
+              Whether you are looking to print some fancy new business cards, or you need a major printing job done for your next marketing event – no job is too big or small for us.
+             </div>
+            </Link>
+          </div>
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
             <Link
               className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
               href="https://create.t3.gg/en/usage/first-steps"
               target="_blank"
             >
-              <h3 className="text-2xl font-bold">First Steps →</h3>
+              <h3 className="text-2xl font-bold">Shop →</h3>
               <div className="text-lg">
-                Just the basics - Everything you need to know to set up your
-                database and authentication.
+              Shop for unique gifts, personalized items, vintage clothing, and more from independent creators. 
+              <br />
+              Discover the latest trends
               </div>
             </Link>
             <Link
@@ -34,16 +57,17 @@ export default async function Home() {
               href="https://create.t3.gg/en/introduction"
               target="_blank"
             >
-              <h3 className="text-2xl font-bold">Documentation →</h3>
+              <h3 className="text-2xl font-bold ">Cloudus Community →</h3>
               <div className="text-lg">
-                Learn more about Create T3 App, the libraries it uses, and how
-                to deploy it.
+                Blog, Share your craft, Share your moments, Learn and participate in OpenSauce Projects and how to deploy them.
               </div>
             </Link>
           </div>
+
+
           <div className="flex flex-col items-center gap-2">
             <p className="text-2xl text-white">
-              {hello ? hello.greeting : "Loading tRPC query..."}
+              {hello ? hello.greeting : "Loading Cloudus Servers..."}
             </p>
 
             <div className="flex flex-col items-center justify-center gap-4">
